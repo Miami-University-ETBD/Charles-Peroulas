509e718bca81b45d6de8b384972e485b59a765c0
charninja1 <156854104+charninja1@users.noreply.github.com>
2024-10-30 13:58:22 -0400

    Restored Changes

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Welcome to Miami University Dining Options!");
+    const contactLink = document.getElementById("contact");
+    contactLink.addEventListener("click", () => {
+        alert("You clicked the Contact Us link!");
+    });
+});
